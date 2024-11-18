190207669cb7da9f55062b82730d5fcc98ea7f36
Cosmos1245 <167671233+Cosmos1245@users.noreply.github.com>
2024-11-18 15:15:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from './project.js';
+
+function App() {
+  return (
+    <div className="App">       
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App;
+
