45e46e8da8a0a1fb81ba2d05431c3a8b3d93462f
Cosmos1245 <167671233+Cosmos1245@users.noreply.github.com>
2024-11-25 08:37:25 +0530

    Update Collections.js

--- src/pages/Collections.js
+++ src/pages/Collections.js
@@ -19,7 +19,7 @@ const CarCollection = () => {
     useEffect(() => {
         const fetchCars = async () => {
             try {
-              const response = await axios.get('https://universal-dynamics-backend.onrender.com/cars');
+              const response = await axios.get('https://universal-dynamics-backend.onrender.com/brands');
 
                 if (Array.isArray(response.data)) {
                     setCars(response.data);
